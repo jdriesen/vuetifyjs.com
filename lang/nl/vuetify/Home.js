b5ab01007239def507d69745a6a9e2c556eba2df
Johnny Driesen <info@3sn.be>
2018-01-05 18:29:18 +0100

    Translated all Files in /lang/nl/Vuetify Folder

---
+++ lang/nl/vuetify/Home.js
@@ -0,0 +1,52 @@
+export default {
+  "announcementBanner": "Aww yisss, dit is een geweldige aankondiging! Of het is misschien zelfs een advertentie !",
+  "heading1": "Vuetify",
+  "heading1cont": "Material Component Framework",
+  "getStarted": "Hoe beginnen ?",
+  "features": [
+    {
+      "img": "/static/doc-images/feature1.svg",
+      "title": "Ready-Made Project Scaffolding",
+      "text": "Vuetify biedt 8 pre-made vue-cli templates. Van eenvoundige html tot full-blown SSR. De setup duurt slechts enkele minuten."
+    },
+    {
+      "img": "/static/doc-images/feature2.svg",
+      "title": "Semantic Material Componenten",
+      "text": "Vueitfy biedt u een 'armade' van gespecialiseerde componenten. Er zijn er meer dan 80; er is een oplossing voor elk type applicatie."
+    },
+    {
+      "img": "/static/doc-images/feature3.svg",
+      "title": "Levendige Community",
+      "text": "Wanneer je met een vraag zit, wens je zo snel mogelijk een oplossing. Vuetify biedt chat support dmv een groeiende community op Discord."
+    }
+  ],
+  "checkFeaturesTitle": "Alle Tools die je nodig hebt..",
+  "checkFeaturesTitleCtd": "Om gelooflijk knappe Gebruiker-Inerfaces te bouwen",
+  "checkFeatures": [
+    "Community Chat Support",
+    "Dynamische Themas",
+    "SSR en PWA Support",
+    "8 vue-cli Templates"
+  ],
+  "checkFeaturesCtd": [
+    "80+ Material Design Componenten",
+    "A La Carte Importing",
+    "Transition Componenten",
+    "Ongelooflijk Snel",
+  ],
+  "letterFromAuthor": [
+    "Hi,",
+    "Ik weet dat het kiezen van een UI framework geen eenvoudige taak is -- er zijn enorm veel opties op de markt, en elk van hen claimt de beste te zijn. Je dient dus de beslissing zware te nemen: Wat zijn de paramaters voor een succesvolle applicatie?",
+    "Dit was ongeveer een jaar geleden.. Zoals gezegd, de keuze is enorm, doch geen enkele had 'alles' wat ik nodig had, dus ik heb de beslissing genomen om aan Vuetify te beginnening werken.",
+    "Ik ben blij te zien dat je overweesgt om Vuetify te gebruiken als jouw volgend UI framework. Er werd veel aandacht besteed om de perfecte setup te bouwen, startende van een idee tot de uiteindelijke productie. Ik kijk ernaar uit om jou te begroeten in de Vuetify community, en ben nieuwsgierig te zien welke interfaces jij zal creeren."
+  ],
+  "authorOfVuetify": "Vuetify Auteur",
+  "support": {
+    "hasVuetifyHelped": "Heeft Vuetify jou geholpen om een mooie applicatie te creeren",
+    "showYourSupport": "Geef ons jouw support door",
+    "becomingAPatron":  "een Vuetify patron te worden"
+  },
+  "becomeSponsor": "Word Sponsor",
+  "proudlySponsoredBy": "Patreon Sponsors",
+  "callout": "Supercharge jouw software-ontwikkelingen met de kracht van Vue en Material Design"
+}
---
+++ lang/nl/vuetify/PremadeThemes.js
@@ -0,0 +1,32 @@
+export default {
+  header: 'Gratis en Premium themas',
+  headerText: 'Vuetify biedt Gratis en Premium themas om snel van start te gaan.. Gratis themes zijn beschikbaar via vue-cli of je kan eenvoudig de broncode downloaden.',
+  free: 'FREE',
+  demo: 'Demo',
+  comingSoon: 'Word verwacht',
+  sourceCode: 'Bron Code',
+  templates: [
+    {
+      screenshot: '/static/doc-images/starter/vuetify-premium.jpg',
+      title: 'Startup',
+      tag: 'PREMIUM',
+      description: 'Toon jouw Firma of persoonlijke portfolio met deze hand-gemaakte Material design template.',
+      price: '25$',
+      demoUrl: ''
+    },
+    {
+      screenshot: '/static/doc-images/starter/vuetify-parallax-starter.png',
+      title: 'Parallax',
+      description:'Deze Single pagina parallax kan een geweldige home pagina zijn voor elk type applicatie.',
+      sourceUrl: 'vuetifyjs/parallax-starter',
+      demoUrl: '/themes/parallax-starter'
+    },
+    {
+      screenshot: '/static/doc-images/starter/vuetify-blog-starter.png',
+      title: 'Blog',
+      description:'Deze eenvoudige template biedt een cleane interface voor een blog-like applicatie.',
+      sourceUrl: 'vuetifyjs/blog-starter',
+      demoUrl: '/themes/blog-starter'
+    }
+  ]
+}
