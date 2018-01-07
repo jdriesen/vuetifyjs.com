80451aca020aff2a8ddee5acc970821495388a52
Johnny Driesen <info@3sn.be>
2018-01-07 13:19:06 +0100

    Included /lang/nl/index.js in commit. (Forgot this one in prev commits...My bad...)

---
+++ lang/nl/index.js
@@ -0,0 +1,43 @@
+// Pages
+
+
+import Vuetify from './vuetify'
+/* --- Start ToDo
+import GettingStarted from './getting-started'
+import Guides from './guides'
+*/
+import Layout from './layout'
+/*
+import Style from './style'
+import Motion from './motion'
+// End ToDo */
+ 
+
+// Generic
+import Generic from './generic'
+
+// Components
+/* --- Start ToDo
+import Components from './components'
+// End ToDo */
+
+// Directives
+import Directives from './directives'
+
+// Mixins
+/* --- Start ToDo
+import Mixins from './mixins'
+// End ToDo */
+
+export default {
+  // Components,
+  Directives,
+  Generic,
+  // GettingStarted,
+  // Guides,
+  Layout,
+  // Mixins,
+  // Motion,
+  // Style,
+  Vuetify
+}
